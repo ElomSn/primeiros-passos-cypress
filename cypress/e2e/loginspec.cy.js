b7ebcb92e5a52a332a4f4ef9be5a8449a2007c0c
Elom sena <elomsena1@gmail.com>
2025-05-21 14:47:47 -0300

    atualização redme

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,8 +1,19 @@
 describe('Orange HRM Tests', () => {
+
+  const selectorslist = {
+
+    usernameField: "[name='username']"
+  }
+
+
+
+
+
   it('Login - success', () => {
+
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get(selectorslist.usernameField).type('Admin')
+    cy.get("[name='password']").type('admin123')
     cy.get('.oxd-button').click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboar')
@@ -10,10 +21,10 @@ describe('Orange HRM Tests', () => {
 
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert')
+    cy.get("[name='username']").type('Test')
+    cy.get("[name='password']").type('Test')
+    cy.get("[type='submit").click()
+    cy.get("[role='alert'")
 
   })
 
