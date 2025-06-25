a578ec4309cbae608fa57077c74539833654a9cd
Elom sena <elomsena1@gmail.com>
2025-06-24 21:06:27 -0300

    base url added

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -16,7 +16,7 @@ describe('Orange HRM Tests', () => {
 
   it('Login - success', () => {
 
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passowordFild).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -25,7 +25,7 @@ describe('Orange HRM Tests', () => {
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passowordFild).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
